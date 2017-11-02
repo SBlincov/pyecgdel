c129f6f244535e8be0fefe6f9324cfded17aa723
Aaron <aaron.blare@mail.ru>
2017-11-03 01:38:52 +0300

    test set

--- Source/Scenario/db_set_single.py
+++ Source/Scenario/db_set_single.py
@@ -36,7 +36,7 @@ for lead_name in leads_names:
     columns_names.append("json_" + lead_name + "_characteristics")
 
 
-record_name = 'record_2843'
+record_name = 'record_2318'
 
 d = {}
 local_path = DBConfig.get_db_path() + "\\" + record_name + "\\"
@@ -52,52 +52,53 @@ if file_id is not -1:
     print(': file_id: ', file_id)
 
     for lead in cb.leads:
-        print(lead)
+        if lead == "i":
+            print(lead)
 
-        filtrated = np.loadtxt(local_path + '/lead_' + lead + '/filtrated.txt')
-        d["json_lead_" + lead + "_filtrated"] = [(file_id, filtrated.tolist())]
+            filtrated = np.loadtxt(local_path + '/lead_' + lead + '/filtrated.txt')
+            d["json_lead_" + lead + "_filtrated"] = [(file_id, filtrated.tolist())]
 
-        p_delineation = np.loadtxt(local_path + '/lead_' + lead + '/p_delineation.txt', dtype=np.int)
-        print("p_del shape: ", p_delineation.shape)
-        if len(p_delineation) is 0:
-            d["json_lead_" + lead + "_p_delineation"] = [(file_id, [])]
-        elif len(p_delineation) is 1:
-            d["json_lead_" + lead + "_p_delineation"] = [(file_id, [p_delineation.tolist()])]
-        else:
-            d["json_lead_" + lead + "_p_delineation"] = [(file_id, p_delineation.tolist())]
-
-        qrs_delineation = np.loadtxt(local_path + '/lead_' + lead + '/qrs_delineation.txt', dtype=np.int)
-        print("qrs_del shape: ", qrs_delineation.shape)
-        if len(qrs_delineation) is 0:
-            d["json_lead_" + lead + "_qrs_delineation"] = [(file_id, [])]
-        elif len(qrs_delineation) is 1:
-            d["json_lead_" + lead + "_qrs_delineation"] = [(file_id, [qrs_delineation.tolist()])]
-        else:
-            d["json_lead_" + lead + "_qrs_delineation"] = [(file_id, qrs_delineation.tolist())]
-
-        t_delineation = np.loadtxt(local_path + '/lead_' + lead + '/t_delineation.txt', dtype=np.int)
-        print("t_del shape: ", t_delineation.shape)
-        if len(t_delineation) is 0:
-            d["json_lead_" + lead + "_t_delineation"] = [(file_id, [])]
-        elif len(t_delineation) is 1:
-            d["json_lead_" + lead + "_t_delineation"] = [(file_id, [t_delineation.tolist()])]
-        else:
-            d["json_lead_" + lead + "_t_delineation"] = [(file_id, t_delineation.tolist())]
-
-        print("\n")
-
-        char_path = local_path + '/lead_' + lead + '/characteristics.txt'
-        lines = [line.rstrip('\n') for line in open(char_path)]
-        ch_names = []
-        ch_values = []
-        for line in lines:
-            ch = line.split()
-            ch_names.append(ch[0])
-            if (ch[1]) != 'n':
-                ch_values.append(float(ch[1]))
+            p_delineation = np.loadtxt(local_path + '/lead_' + lead + '/p_delineation.txt', dtype=np.int)
+            print("p_del shape: ", p_delineation.shape)
+            if len(p_delineation) is 0:
+                d["json_lead_" + lead + "_p_delineation"] = [(file_id, [])]
+            elif len(p_delineation) is 1:
+                d["json_lead_" + lead + "_p_delineation"] = [(file_id, [p_delineation.tolist()])]
             else:
-                ch_values.append(None)
-        d["json_lead_" + lead + "_characteristics"] = [(file_id, [ch_names, ch_values])]
+                d["json_lead_" + lead + "_p_delineation"] = [(file_id, p_delineation.tolist())]
+
+            qrs_delineation = np.loadtxt(local_path + '/lead_' + lead + '/qrs_delineation.txt', dtype=np.int)
+            print("qrs_del shape: ", qrs_delineation.shape)
+            if len(qrs_delineation) is 0:
+                d["json_lead_" + lead + "_qrs_delineation"] = [(file_id, [])]
+            elif len(qrs_delineation) is 1:
+                d["json_lead_" + lead + "_qrs_delineation"] = [(file_id, [qrs_delineation.tolist()])]
+            else:
+                d["json_lead_" + lead + "_qrs_delineation"] = [(file_id, qrs_delineation.tolist())]
+
+            t_delineation = np.loadtxt(local_path + '/lead_' + lead + '/t_delineation.txt', dtype=np.int)
+            print("t_del shape: ", t_delineation.shape)
+            if len(t_delineation) is 0:
+                d["json_lead_" + lead + "_t_delineation"] = [(file_id, [])]
+            elif len(t_delineation) is 1:
+                d["json_lead_" + lead + "_t_delineation"] = [(file_id, [t_delineation.tolist()])]
+            else:
+                d["json_lead_" + lead + "_t_delineation"] = [(file_id, t_delineation.tolist())]
+
+            print("\n")
+
+            char_path = local_path + '/lead_' + lead + '/characteristics.txt'
+            lines = [line.rstrip('\n') for line in open(char_path)]
+            ch_names = []
+            ch_values = []
+            for line in lines:
+                ch = line.split()
+                ch_names.append(ch[0])
+                if (ch[1]) != 'n':
+                    ch_values.append(float(ch[1]))
+                else:
+                    ch_values.append(None)
+            d["json_lead_" + lead + "_characteristics"] = [(file_id, [ch_names, ch_values])]
 
     cb.bulk_data_set(d)
     cb.commit()
