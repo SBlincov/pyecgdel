ed076f1e75b82d41bdaaaf1e3bd2b106e106848d
Unknown <hewitt.archie@yandex.ru>
2018-03-12 19:11:23 +0300

    problem detected

--- Source/Model/main/ecg/ecg.py
+++ Source/Model/main/ecg/ecg.py
@@ -96,7 +96,7 @@ class ECG:
             print("QRS delineation " + str(self.leads[lead_id].name) + " complete")
         print("")
 
-        if len(self.leads) > 1:
+        if len(self.leads) > 3:
             qrs_multi_lead_processing(self.leads)
 
         for lead_id in range(0, len(self.leads)):
