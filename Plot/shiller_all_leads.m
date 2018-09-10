fafc2615dea3f561d52b4065df7e5832d7a7e448
Unknown <hewitt.archie@yandex.ru>
2018-09-10 14:37:08 +0300

    bug fixing

--- Plot/shiller_all_leads.m
+++ Plot/shiller_all_leads.m
@@ -1,15 +1,16 @@
 clear all;
 
-base_name = 'shiller';
+base_name = 'shiller_long';
 freq = 500.0;
 
-record = 2319;
+record = 2517;
 
 leads = {'lead_i', 'lead_ii', 'lead_iii', 'lead_avr', 'lead_avl', 'lead_avf', 'lead_v1', 'lead_v2', 'lead_v3', 'lead_v4', 'lead_v5', 'lead_v6'};
-lead_ids = 1:1;
+lead_ids = 1:12;
 
-is_filtered = 1;
-wdc_scales = [1 2 3 4 5];
+is_filtered = 0;
+is_del = 0;
+wdc_scales = [1 3 4 5];
 x_axis_type = 0;
 
 markerWidth = 10;
@@ -52,195 +53,199 @@ for i = 1:size(lead_ids, 2)
     grid on;
     hold all;
     
-    file_name_qrs_del = sprintf('%s%s%sqrs_delineation.txt', db_path, record_path, lead_path);
-    qrs_del = importdata(file_name_qrs_del);
-    
-    times_onset     = zeros(size(qrs_del, 1), 1);
-    times_peak      = zeros(size(qrs_del, 1), 1);
-    times_offset    = zeros(size(qrs_del, 1), 1);
-    vals_onset      = zeros(size(qrs_del, 1), 1);
-    vals_peak       = zeros(size(qrs_del, 1), 1);
-    vals_offset     = zeros(size(qrs_del, 1), 1);
-    
-    for i = 1:size(qrs_del, 1)
-        times_onset(i)  = times(qrs_del(i, 1) + 1);
-        times_peak(i)   = times(qrs_del(i, 2) + 1);
-        times_offset(i) = times(qrs_del(i, 3) + 1);
-        
-        vals_onset(i)   = ecg_lead(qrs_del(i, 1) + 1);
-        vals_peak(i)    = ecg_lead(qrs_del(i, 2) + 1);
-        vals_offset(i)  = ecg_lead(qrs_del(i, 3) + 1);
-    end
-    
-    hLine = plot(times_onset, vals_onset,  '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'r');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    hLine = plot(times_peak, vals_peak,  'o', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'r');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    hLine = plot(times_offset, vals_offset, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'r');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    
-    file_name_p_del = sprintf('%s%s%sp_delineation.txt', db_path, record_path, lead_path);
-    p_del = importdata(file_name_p_del);
-    
-    times_onset     = zeros(size(p_del, 1), 1);
-    times_peak      = zeros(size(p_del, 1), 1);
-    times_offset    = zeros(size(p_del, 1), 1);
-    vals_onset      = zeros(size(p_del, 1), 1);
-    vals_peak       = zeros(size(p_del, 1), 1);
-    vals_offset     = zeros(size(p_del, 1), 1);
-    
-    for i = 1:size(p_del, 1)
-        times_onset(i)  = times(p_del(i, 1) + 1);
-        times_peak(i)   = times(p_del(i, 2) + 1);
-        times_offset(i) = times(p_del(i, 3) + 1);
-        
-        vals_onset(i)   = ecg_lead(p_del(i, 1) + 1);
-        vals_peak(i)    = ecg_lead(p_del(i, 2) + 1);
-        vals_offset(i)  = ecg_lead(p_del(i, 3) + 1);
-    end
-    
-    hLine = plot(times_onset, vals_onset,  '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'green');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    hLine = plot(times_peak, vals_peak,  'o', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'green');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    hLine = plot(times_offset, vals_offset, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'green');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    
-    file_name_t_del = sprintf('%s%s%st_delineation.txt', db_path, record_path, lead_path);
-    t_del = importdata(file_name_t_del);
-    
-    times_onset     = zeros(size(t_del, 1), 1);
-    times_peak      = zeros(size(t_del, 1), 1);
-    times_offset    = zeros(size(t_del, 1), 1);
-    vals_onset      = zeros(size(t_del, 1), 1);
-    vals_peak       = zeros(size(t_del, 1), 1);
-    vals_offset     = zeros(size(t_del, 1), 1);
-    
-    for i = 1:size(t_del, 1)
-        times_onset(i)      = times(t_del(i, 1) + 1);
-        times_peak(i)       = times(t_del(i, 2) + 1);
-        times_offset(i)     = times(t_del(i, 3) + 1);
-        
-        vals_onset(i)       = ecg_lead(t_del(i, 1) + 1);
-        vals_peak(i)        = ecg_lead(t_del(i, 2) + 1);
-        vals_offset(i)      = ecg_lead(t_del(i, 3) + 1);
-    end
-    
-    hLine = plot(times_onset, vals_onset,  '>', 'MarkerSize', markerWidth, 'Color',  'k', 'LineWidth', 1, 'MarkerFaceColor', 'cyan');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    hLine = plot(times_peak, vals_peak,  'o', 'MarkerSize', markerWidth, 'Color',  'k', 'LineWidth', 1, 'MarkerFaceColor', 'cyan');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    hLine = plot(times_offset, vals_offset, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'cyan');
-    set(gca, 'FontSize', 18);
-    set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-    hold all;
-    
-    if exist(sprintf('%s%s/original_del/', db_path, record_path), 'dir')
-    
-        file_name = sprintf('%s%s/original_del/json_R_P_ON.txt', db_path, record_path);
-        data = importdata(file_name);
-        json_r_p_on_indexes = zeros(size(data,1), 1);
-        json_r_p_on_times = zeros(size(data,1), 1);
-        json_r_p_on_vals = zeros(size(data,1), 1);
-        for dump_id = 1:size(data,1)
-            json_r_p_on_indexes(dump_id) = floor(data(dump_id) * freq);
-            json_r_p_on_times(dump_id) = times(json_r_p_on_indexes(dump_id) + 1);
-            json_r_p_on_vals(dump_id) = ecg_lead(json_r_p_on_indexes(dump_id) + 1);
+    if is_del == 1
+        file_name_qrs_del = sprintf('%s%s%sqrs_delineation.txt', db_path, record_path, lead_path);
+        qrs_del = importdata(file_name_qrs_del);
+
+        times_onset     = zeros(size(qrs_del, 1), 1);
+        times_peak      = zeros(size(qrs_del, 1), 1);
+        times_offset    = zeros(size(qrs_del, 1), 1);
+        vals_onset      = zeros(size(qrs_del, 1), 1);
+        vals_peak       = zeros(size(qrs_del, 1), 1);
+        vals_offset     = zeros(size(qrs_del, 1), 1);
+
+        for i = 1:size(qrs_del, 1)
+            times_onset(i)  = times(qrs_del(i, 1) + 1);
+            times_peak(i)   = times(qrs_del(i, 2) + 1);
+            times_offset(i) = times(qrs_del(i, 3) + 1);
+
+            vals_onset(i)   = ecg_lead(qrs_del(i, 1) + 1);
+            vals_peak(i)    = ecg_lead(qrs_del(i, 2) + 1);
+            vals_offset(i)  = ecg_lead(qrs_del(i, 3) + 1);
         end
 
-        hLine = plot(json_r_p_on_times, json_r_p_on_vals, '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [1 0.84 0]);
+        hLine = plot(times_onset, vals_onset,  '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'r');
         set(gca, 'FontSize', 18);
         set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
         hold all;
 
-        file_name = sprintf('%s%s/original_del/json_R_T_OFF.txt', db_path, record_path);
-        data = importdata(file_name);
-        json_r_t_off_indexes = zeros(size(data,1), 1);
-        json_r_t_off_times = zeros(size(data,1), 1);
-        json_r_t_off_vals = zeros(size(data,1), 1);
-        for dump_id = 1:size(data,1)
-            json_r_t_off_indexes(dump_id) = floor(data(dump_id) * freq);
-            json_r_t_off_times(dump_id) = times(json_r_t_off_indexes(dump_id) + 1);
-            json_r_t_off_vals(dump_id) = ecg_lead(json_r_t_off_indexes(dump_id) + 1);
-        end
-
-        hLine = plot(json_r_t_off_times, json_r_t_off_vals, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [1 0.84 0]);
+        hLine = plot(times_peak, vals_peak,  'o', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'r');
         set(gca, 'FontSize', 18);
         set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
         hold all;
 
-        file_name = sprintf('%s%s/original_del/json_R_QRS_ON.txt', db_path, record_path);
-        data = importdata(file_name);
-        json_r_qrs_on_indexes = zeros(size(data,1), 1);
-        json_r_qrs_on_times = zeros(size(data,1), 1);
-        json_r_qrs_on_vals = zeros(size(data,1), 1);
-        for dump_id = 1:size(data,1)
-            json_r_qrs_on_indexes(dump_id) = floor(data(dump_id) * freq);
-            json_r_qrs_on_times(dump_id) = times(json_r_qrs_on_indexes(dump_id) + 1);
-            json_r_qrs_on_vals(dump_id) = ecg_lead(json_r_qrs_on_indexes(dump_id) + 1);
-        end
-
-        hLine = plot(json_r_qrs_on_times, json_r_qrs_on_vals, '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [0.85 0.7 1]);
+        hLine = plot(times_offset, vals_offset, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'r');
         set(gca, 'FontSize', 18);
         set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
         hold all;
 
-        file_name = sprintf('%s%s/original_del/json_R_QRS_OFF.txt', db_path, record_path);
-        data = importdata(file_name);
-        json_r_qrs_off_indexes = zeros(size(data,1), 1);
-        json_r_qrs_off_times = zeros(size(data,1), 1);
-        json_r_qrs_off_vals = zeros(size(data,1), 1);
-        for dump_id = 1:size(data,1)
-            json_r_qrs_off_indexes(dump_id) = floor(data(dump_id) * freq);
-            json_r_qrs_off_times(dump_id) = times(json_r_qrs_off_indexes(dump_id) + 1);
-            json_r_qrs_off_vals(dump_id) = ecg_lead(json_r_qrs_off_indexes(dump_id) + 1);
+
+        file_name_p_del = sprintf('%s%s%sp_delineation.txt', db_path, record_path, lead_path);
+        p_del = importdata(file_name_p_del);
+
+        times_onset     = zeros(size(p_del, 1), 1);
+        times_peak      = zeros(size(p_del, 1), 1);
+        times_offset    = zeros(size(p_del, 1), 1);
+        vals_onset      = zeros(size(p_del, 1), 1);
+        vals_peak       = zeros(size(p_del, 1), 1);
+        vals_offset     = zeros(size(p_del, 1), 1);
+
+        for i = 1:size(p_del, 1)
+            times_onset(i)  = times(p_del(i, 1) + 1);
+            times_peak(i)   = times(p_del(i, 2) + 1);
+            times_offset(i) = times(p_del(i, 3) + 1);
+
+            vals_onset(i)   = ecg_lead(p_del(i, 1) + 1);
+            vals_peak(i)    = ecg_lead(p_del(i, 2) + 1);
+            vals_offset(i)  = ecg_lead(p_del(i, 3) + 1);
         end
 
-        hLine = plot(json_r_qrs_off_times, json_r_qrs_off_vals, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [0.85 0.7 1]);
+        hLine = plot(times_onset, vals_onset,  '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'green');
         set(gca, 'FontSize', 18);
         set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-        hold all; 
-    end
-    
-    
-    file_name_wdc = sprintf('%s%s%swdc.txt', db_path, record_path, lead_path);
-    wdc = importdata(file_name_wdc);
-    
-    wdc_num = size(wdc, 2);
-    
-    for wdc_scale_id = 1:size(wdc_scales, 2)
+        hold all;
+
+        hLine = plot(times_peak, vals_peak,  'o', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'green');
+        set(gca, 'FontSize', 18);
+        set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+        hold all;
+
+        hLine = plot(times_offset, vals_offset, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'green');
+        set(gca, 'FontSize', 18);
+        set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+        hold all;
+
+
+        file_name_t_del = sprintf('%s%s%st_delineation.txt', db_path, record_path, lead_path);
+        t_del = importdata(file_name_t_del);
+
+        times_onset     = zeros(size(t_del, 1), 1);
+        times_peak      = zeros(size(t_del, 1), 1);
+        times_offset    = zeros(size(t_del, 1), 1);
+        vals_onset      = zeros(size(t_del, 1), 1);
+        vals_peak       = zeros(size(t_del, 1), 1);
+        vals_offset     = zeros(size(t_del, 1), 1);
+
+        for i = 1:size(t_del, 1)
+            times_onset(i)      = times(t_del(i, 1) + 1);
+            times_peak(i)       = times(t_del(i, 2) + 1);
+            times_offset(i)     = times(t_del(i, 3) + 1);
+
+            vals_onset(i)       = ecg_lead(t_del(i, 1) + 1);
+            vals_peak(i)        = ecg_lead(t_del(i, 2) + 1);
+            vals_offset(i)      = ecg_lead(t_del(i, 3) + 1);
+        end
+
+        hLine = plot(times_onset, vals_onset,  '>', 'MarkerSize', markerWidth, 'Color',  'k', 'LineWidth', 1, 'MarkerFaceColor', 'cyan');
+        set(gca, 'FontSize', 18);
+        set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+        hold all;
+
+        hLine = plot(times_peak, vals_peak,  'o', 'MarkerSize', markerWidth, 'Color',  'k', 'LineWidth', 1, 'MarkerFaceColor', 'cyan');
+        set(gca, 'FontSize', 18);
+        set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+        hold all;
+
+        hLine = plot(times_offset, vals_offset, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', 'cyan');
+        set(gca, 'FontSize', 18);
+        set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+        hold all;
+
+        if exist(sprintf('%s%s/original_del/', db_path, record_path), 'dir')
+
+            file_name = sprintf('%s%s/original_del/json_R_P_ON.txt', db_path, record_path);
+            data = importdata(file_name);
+            json_r_p_on_indexes = zeros(size(data,1), 1);
+            json_r_p_on_times = zeros(size(data,1), 1);
+            json_r_p_on_vals = zeros(size(data,1), 1);
+            for dump_id = 1:size(data,1)
+                json_r_p_on_indexes(dump_id) = floor(data(dump_id) * freq);
+                json_r_p_on_times(dump_id) = times(json_r_p_on_indexes(dump_id) + 1);
+                json_r_p_on_vals(dump_id) = ecg_lead(json_r_p_on_indexes(dump_id) + 1);
+            end
+
+            hLine = plot(json_r_p_on_times, json_r_p_on_vals, '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [1 0.84 0]);
+            set(gca, 'FontSize', 18);
+            set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+            hold all;
+
+            file_name = sprintf('%s%s/original_del/json_R_T_OFF.txt', db_path, record_path);
+            data = importdata(file_name);
+            json_r_t_off_indexes = zeros(size(data,1), 1);
+            json_r_t_off_times = zeros(size(data,1), 1);
+            json_r_t_off_vals = zeros(size(data,1), 1);
+            for dump_id = 1:size(data,1)
+                json_r_t_off_indexes(dump_id) = floor(data(dump_id) * freq);
+                json_r_t_off_times(dump_id) = times(json_r_t_off_indexes(dump_id) + 1);
+                json_r_t_off_vals(dump_id) = ecg_lead(json_r_t_off_indexes(dump_id) + 1);
+            end
+
+            hLine = plot(json_r_t_off_times, json_r_t_off_vals, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [1 0.84 0]);
+            set(gca, 'FontSize', 18);
+            set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+            hold all;
+
+            file_name = sprintf('%s%s/original_del/json_R_QRS_ON.txt', db_path, record_path);
+            data = importdata(file_name);
+            json_r_qrs_on_indexes = zeros(size(data,1), 1);
+            json_r_qrs_on_times = zeros(size(data,1), 1);
+            json_r_qrs_on_vals = zeros(size(data,1), 1);
+            for dump_id = 1:size(data,1)
+                json_r_qrs_on_indexes(dump_id) = floor(data(dump_id) * freq);
+                json_r_qrs_on_times(dump_id) = times(json_r_qrs_on_indexes(dump_id) + 1);
+                json_r_qrs_on_vals(dump_id) = ecg_lead(json_r_qrs_on_indexes(dump_id) + 1);
+            end
+
+            hLine = plot(json_r_qrs_on_times, json_r_qrs_on_vals, '>', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [0.85 0.7 1]);
+            set(gca, 'FontSize', 18);
+            set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+            hold all;
+
+            file_name = sprintf('%s%s/original_del/json_R_QRS_OFF.txt', db_path, record_path);
+            data = importdata(file_name);
+            json_r_qrs_off_indexes = zeros(size(data,1), 1);
+            json_r_qrs_off_times = zeros(size(data,1), 1);
+            json_r_qrs_off_vals = zeros(size(data,1), 1);
+            for dump_id = 1:size(data,1)
+                json_r_qrs_off_indexes(dump_id) = floor(data(dump_id) * freq);
+                json_r_qrs_off_times(dump_id) = times(json_r_qrs_off_indexes(dump_id) + 1);
+                json_r_qrs_off_vals(dump_id) = ecg_lead(json_r_qrs_off_indexes(dump_id) + 1);
+            end
+
+            hLine = plot(json_r_qrs_off_times, json_r_qrs_off_vals, '<', 'MarkerSize', markerWidth, 'Color', 'k', 'LineWidth', 1, 'MarkerFaceColor', [0.85 0.7 1]);
+            set(gca, 'FontSize', 18);
+            set(get(get(hLine,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+            hold all; 
+        end
         
-        wdc_scale = wdc_scales(wdc_scale_id);
         
-        curr_wdc = wdc(:,wdc_scale);
-        hLine = plot(times(1:size(wdc, 1)), curr_wdc, 'LineWidth', 0.5);
-        legend(hLine, sprintf('wdc #%d', wdc_scale));
-        set(gca, 'FontSize', 18);
-        hold all;
+        
+        file_name_wdc = sprintf('%s%s%swdc.txt', db_path, record_path, lead_path);
+        wdc = importdata(file_name_wdc);
+        
+        wdc_num = size(wdc, 2);
+        
+        for wdc_scale_id = 1:size(wdc_scales, 2)
+            
+            wdc_scale = wdc_scales(wdc_scale_id);
+            
+            curr_wdc = wdc(:,wdc_scale);
+            hLine = plot(times(1:size(wdc, 1)), curr_wdc, 'LineWidth', 0.5);
+            legend(hLine, sprintf('wdc #%d', wdc_scale));
+            set(gca, 'FontSize', 18);
+            hold all;
+        end
+        
     end
     
     grid on;
